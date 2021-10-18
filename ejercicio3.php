44b73c2901a3f8090fd0f200aa222b30b2958963
SamuelSEV <samuelsev26@gmail.com>
2021-10-18 23:07:45 +0200

    Ejercicio 3 terminado

--- ejercicio3.php
+++ ejercicio3.php
@@ -10,10 +10,40 @@
     <?php
         $informacion = simplexml_load_file("datos.xml");
         printf ("Informacion libro 2: <br> AUTOR: %s <br> TITULO: %s <br> GENERO: %s <br> PRECIO: %s <br> PUBLICACION: %s <br> DESCRIPCION: %s <br> " , $informacion->book[1]->author, $informacion->book[1]->title, $informacion->book[1]->genre, $informacion->book[1]->price, $informacion->book[1]->publish_date, $informacion->book[1]->description);
-        foreach($informacion as $book=>$valor){
-            printf ("Informacion libro 2: <br> AUTOR: %s <br> TITULO: %s <br> GENERO: %s <br> PRECIO: %s <br> PUBLICACION: %s <br> DESCRIPCION: %s <br> " , $informacion->book[$book]->author, $informacion->book[$book]->title, $informacion->book[$book]->genre, $informacion->book[$book]->price, $informacion->book[$book]->publish_date, $informacion->book[$book]->description);
+        printf ("<table border=1>");
+        printf ("<tr>");
+        printf ("<th>Titulo</th>" . "<th>Genero</th>" . "<th>Precio</th>") ;
+        printf ("</tr>");
+        
+        foreach($informacion as $book){
+            printf ("<tr>");
+            printf ("<td>%s</td>",$book->title);
+            printf ("<td>%s</td>",$book->genre);
+            printf ("<td>%s</td>",$book->price);
+            printf ("<tr>");
         }
-            
+        printf ("</table>");   
+        $catalog=$informacion->addChild("book");
+        $book=$catalog->addChild("author","J.R.R. TOLKIEN");
+        $book=$catalog->addChild("title","Las dos torres");
+        $book=$catalog->addChild("genre","Fantasia");
+        $book=$catalog->addChild("price","10.40");
+        $book=$catalog->addChild("publish_date","1954-11-11");
+        $book=$catalog->addChild("description","segundo volumen de la novela de fantasía heroica El Señor de los Anillos");
+        $informacion->asXML("datos-xml");
+        printf ("<table border=1>");
+        printf ("<tr>");
+        printf ("<th>Titulo</th>" . "<th>Genero</th>" . "<th>Precio</th>" );
+        printf ("</tr>");
+        
+        foreach($informacion as $book){
+            printf ("<tr>");
+            printf ("<td>%s</td>",$book->title);
+            printf ("<td>%s</td>",$book->genre);
+            printf ("<td>%s</td>",$book->price);
+            printf ("<tr>");
+        }
+        printf ("</table>");   
 
     ?>
 </body>
